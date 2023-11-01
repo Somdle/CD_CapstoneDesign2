5937811d25ed475a25a4bf747d1bb145a3fc576e
Somdle <58317426+Somdle@users.noreply.github.com>
2023-11-01 23:03:02 +0900

    제한설정 및 해제

--- www_server/backend/insert_hotel_info.php
+++ www_server/backend/insert_hotel_info.php
@@ -5,15 +5,6 @@ header("Access-Control-Allow-Methods: POST, GET, OPTIONS");
 header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, X-Requested-With");
 
 /****************************************************************
- * 기능: 비 정상 접근 차단, JSON 데이터 변환
- * 설명: 사용자가 로그인 되어 있지 않은 경우 로그인 페이지로 리다이렉트
- ****************************************************************/
-if($_SERVER["REQUEST_METHOD"] != "POST"){ // post 통신이 아니면
-    echo "<script>location.replace('index.php');</script>";
-    die("비정상접근");
-}
-
-/****************************************************************
  * 기능: 데이터베이스 관련
  * 설명: 
  ****************************************************************/
