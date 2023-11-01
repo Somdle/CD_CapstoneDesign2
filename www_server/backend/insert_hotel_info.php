9d4e9ac075944b65f75ac5f4a5b2850da41d38c4
Somdle <58317426+Somdle@users.noreply.github.com>
2023-11-01 10:37:20 +0900

    서버측 코드 작성 완료

---
+++ www_server/backend/insert_hotel_info.php
@@ -0,0 +1,85 @@
+<?php
+/****************************************************************
+ * 기능: 비 정상 접근 차단, JSON 데이터 변환
+ * 설명: 사용자가 로그인 되어 있지 않은 경우 로그인 페이지로 리다이렉트
+ ****************************************************************/
+if($_SERVER["REQUEST_METHOD"] != "POST"){ // post 통신이 아니면
+    echo "<script>location.replace('index.php');</script>";
+    die("비정상접근");
+}
+
+/****************************************************************
+ * 기능: 데이터베이스 관련
+ * 설명: 
+ ****************************************************************/
+//절대경로로 접근, Nginx에 DB정보가 미리 준비되어 있어야함
+$db_config = parse_ini_file('/config/nginx/db_config.ini');
+
+// MySQL 데이터베이스 연결 정보
+$servername = $db_config['host'];
+$username   = $db_config['username'];
+$password   = $db_config['password'];
+$dbname     = "capstone2_db"; //$db_config['dbname'];
+
+// 데이터베이스 연결
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// 파일정보 처리
+if(isset($_FILES['fileData'])){
+    $fileData        = isset($_FILES["fileData"]) ? $_FILES["fileData"] : null;
+    $fileDataEncoded = mysqli_real_escape_string($conn, base64_encode(file_get_contents($fileData)));
+
+    $fileName = isset($_POST["fileName"])  ? mysqli_real_escape_string($conn, $_POST["fileName"]) : null;
+    $fileType = isset($_POST["fileType"])  ? mysqli_real_escape_string($conn, $_POST["fileType"]) : null;
+    $fileSize = isset($_POST["fileSize"])  ? mysqli_real_escape_string($conn, $_POST["fileSize"]) : null;
+}
+
+// 기타정보 처리
+$hotelName   = isset($_POST["hotelName"])   ? mysqli_real_escape_string($conn, $_POST["hotelName"])   : null;
+$hotelCharge = isset($_POST["hotelCharge"]) ? mysqli_real_escape_string($conn, $_POST["hotelCharge"]) : null;
+$hotelIntro  = isset($_POST["hotelIntro"])  ? mysqli_real_escape_string($conn, $_POST["hotelIntro"])  : null;
+
+// 트랜잭션 시작
+$conn->begin_transaction();
+
+try {
+    // SQL 쿼리 작성 및 실행
+    $sql  = "INSERT INTO hotel_info_table (hotel_name, hotel_charge, hotel_intro) VALUES (?, ?, ?)";
+    $stmt = $conn->prepare($sql);
+    $stmt->bind_param("sis", $hotelName, $hotelCharge, $hotelIntro);
+    $stmt->execute();
+
+    // 쿼리 성공여부 확인
+    if ($stmt->affected_rows > 0) {
+        $hotel_id = $conn->insert_id; // 방금 추가된 데이터의 hotel_id를 받아옵니다.
+
+        // SQL 쿼리 작성 및 실행
+        $sql = "INSERT INTO hotel_img_table (hotel_id, hotel_img) VALUES (?, ?)";
+        $stmt = $conn->prepare($sql);
+        $stmt->bind_param("ib", $hotel_id, $fileDataEncoded);
+        $stmt->execute();
+
+        // 쿼리 성공여부 확인
+        if ($stmt->affected_rows > 0) {
+            // 모든 작업이 성공하면 트랜잭션 커밋
+            $conn->commit();
+        } else {
+            throw new Exception("이미지 데이터 처리 실패: " . $stmt->error);
+        }
+    } else {
+        throw new Exception("데이터 처리 실패: " . $stmt->error);
+    }
+} catch (Exception $e) {
+    // 작업 중 에러가 발생하면 트랜잭션 롤백
+    $conn->rollback();
+
+    $stmt->close();
+    $conn->close();
+
+    echo json_encode(array("status" => "error", "message" => $e->getMessage()));
+    exit();
+}
+
+// 연결 종료
+$stmt->close();
+$conn->close();
--- www_server/script.js
+++ www_server/script.js
@@ -1,36 +1,6 @@
 async function insertForm(event) {
     event.preventDefault();  // 기본 폼 제출 동작을 막습니다.
 
-    // 선택정보 업로드
-    try {
-        // 데이터 전송 후 응답을 변수에 저장
-        const response = await fetch("./backend/insert_hotel_info.php", {
-            method: "POST",
-            headers: { "Content-Type": "application/x-www-form-urlencoded", }, // 간단한 텍스트 데이터
-            body: new URLSearchParams({
-                hotelName:   document.getElementById("insert-hotel-name").value,
-                hotelCharge: document.getElementById("insert-hotel-charge").value,
-                hotelIntro:  document.getElementById("insert-hotel-intro").value,
-            }).toString(),
-        });
-        
-        // 응답 확인
-        if (response.ok) {
-            const responseData = await response.json(); // 응답을 JSON 형태로 파싱합니다.
-            if(responseData.status == 'success'){
-                console.log("데이터 처리 성공")
-            } else {
-                throw new Error("데이터 처리 실패");
-            }
-        } else {
-            throw new Error("서버 응답 에러");
-        }
-    } catch (error) {
-        console.error("오류정보: ", error);
-        return false;
-    }
-
-    // 이미지 업로드
     try {
         // 전송할 파일 가져오기 및 파일 여부 확인
         const file = document.getElementById("insert-hotel-img").files[0];
@@ -47,10 +17,14 @@ async function insertForm(event) {
         formData.append("fileType", file.type);
         formData.append("fileSize", file.size);
 
+        // 데이터 폼에 텍스트 정보 추가
+        formData.append("hotelName",   document.getElementById("insert-hotel-name").value);
+        formData.append("hotelCharge", document.getElementById("insert-hotel-charge").value);
+        formData.append("hotelIntro",  document.getElementById("insert-hotel-intro").value);
+
         // 데이터 전송 후 응답을 변수에 저장
-        const response = await fetch("./backend/insert_hotel_info_image.php", {
+        const response = await fetch("./backend/insert_hotel_info.php", {
             method: "POST",
-            // headers: { "Content-Type": "multipart/form-data", }, // 파일이나 비 ASCII 데이터, 큰 양의 바이너리 데이터 등을 전송 (여기선 필요하지 않음)
             body: formData,
         });
         
@@ -69,5 +43,4 @@ async function insertForm(event) {
         console.error("오류정보: ", error);
         return false;
     }
-
 }
